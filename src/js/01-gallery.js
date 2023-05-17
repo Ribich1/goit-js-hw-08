74ffc7e591665447ff322876f215a3f28094bbcf
Ribich1 <ribich@ukr.net>
2023-05-17 14:56:47 +0200

    08-1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,26 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
+// Описан в документации
+import SimpleLightbox from "simplelightbox";
+// Дополнительный импорт стилей
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 console.log(galleryItems);
+
+const galleryUl = document.querySelector(".gallery");
+
+
+const markupItem = galleryItems.map(({ preview, original, description }) =>
+    `<li class="gallery__item"><a class="gallery__link" href=${original}> <img 
+   class="gallery__image" src=${preview} alt="${ description }" /> </a></li>`
+).join('');
+
+galleryUl.insertAdjacentHTML('afterbegin', markupItem);
+
+ let gallery = new SimpleLightbox('.gallery a', {
+        captionsData: 'alt',
+        captionDelay: 250
+    });
+
+
