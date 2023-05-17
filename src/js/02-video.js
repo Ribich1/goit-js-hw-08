76c256ab74daff46bf91a5c9decbdfbf349324e1
Ribich1 <ribich@ukr.net>
2023-05-17 17:24:05 +0200

    08-2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,23 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+    const player = new Player(iframe);
+
+    player.on('timeupdate', throttle(onPlay, 1000));
+
+
+function onPlay(evt) {
+    localStorage.setItem("videoplayer-current-time", evt.seconds);
+};
+
+const savedTime = localStorage.getItem("videoplayer-current-time");
+const parseSavedTime = JSON.parse(savedTime);
+
+if (parseSavedTime) {
+    player.setCurrentTime(parseSavedTime)
+};
+
+
+
 
